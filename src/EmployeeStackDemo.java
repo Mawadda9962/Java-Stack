bde87b73743f64eb1fa22ba7dc14c78dbeb5b3c6
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-23 10:56:15 +0400

    adding the employee action records

--- src/EmployeeStackDemo.java
+++ src/EmployeeStackDemo.java
@@ -5,7 +5,17 @@ public class EmployeeStackDemo {
     public static void main(String[] args){
         Stack<String> EmployeeStackDemo = new Stack<>();
 
-        EmployeeStackDemo.push("")
+        EmployeeStackDemo.push("Employee A submitted report");
+        EmployeeStackDemo.push("Employee B updating the data");
+        EmployeeStackDemo.push("Employee c adding the changes");
+        EmployeeStackDemo.push("Employee D completed the report");
+        EmployeeStackDemo.push("Employee E logging out");
+
+        System.out.println("Stack after pushes: " + EmployeeStackDemo);
+
+
+
+
 
 
     }
