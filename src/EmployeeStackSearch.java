0a94b96460331f73965e04453808e540b2137706
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-23 11:30:12 +0400

    Use contains() to check if specific actions exist in the stack

--- src/EmployeeStackSearch.java
+++ src/EmployeeStackSearch.java
@@ -8,7 +8,7 @@ public class EmployeeStackSearch {
         //Creating a stack to store employee actions
         EmployeeStackSearch.push("Employee A submitted report");
         EmployeeStackSearch.push("Employee B updating the data");
-        EmployeeStackSearch.push("Employee c adding the changes");
+        EmployeeStackSearch.push("Employee C adding the changes");
         EmployeeStackSearch.push("Employee D completed the report");
         EmployeeStackSearch.push("Employee E logging out");
         EmployeeStackSearch.push("Employee F Approving the report");
@@ -20,12 +20,27 @@ public class EmployeeStackSearch {
         System.out.println("Top element (peek): " + EmployeeStackSearch.peek());
 
         //Useing search() to find the position of different employee actions
-        int position = EmployeeStackSearch.search("Employee A submitted report");
-        if (position != -1){
-            System.out.println("Element A found at position + position");
+        int position1 = EmployeeStackSearch.search("Employee A submitted report");
+        if (position1 != -1){
+            System.out.println("Element A found at position" + position1);
 
         }else {
             System.out.println("Employee action not found");
         }
+
+        //Useing search() to find the position of second employee actions
+        int position2 = EmployeeStackSearch.search("Employee C adding the changes");
+        if (position2 != -1){
+            System.out.println("Element C found at position " + position2);
+        }else {
+            System.out.println("Employee action not found");
+        }
+
+        //Useing contains() to check if specific actions exist in the stack
+        System.out.println("Contains Employee E logging out" + EmployeeStackSearch.contains("Employee E logging out"));
+
+
+
+
     }
 }
