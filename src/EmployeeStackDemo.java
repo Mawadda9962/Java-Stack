2f9f04599d6cfb71bffd4126bc2dbc65b32cb79f
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-23 10:51:03 +0400

    Solving Employee Stack Demo Task1 adding the class name

---
+++ src/EmployeeStackDemo.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+
+public class EmployeeStackDemo {
+
+    public static void main(String[] args){
+        Stack<String> EmployeeStackDemo = new Stack<>();
+
+        EmployeeStackDemo.push("")
+
+
+    }
+
+}
