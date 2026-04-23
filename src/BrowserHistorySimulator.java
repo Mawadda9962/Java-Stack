1d4e05c342ea75be263ede97fdabade5adcaaebc
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-23 12:00:35 +0400

    storing the web bages

---
+++ src/BrowserHistorySimulator.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class BrowserHistorySimulator {
+
+    public static void main(String[] args){
+        Stack<String> BrowserHistorySimulator = new Stack<>();
+
+        //Creating a stack to store web pages
+        BrowserHistorySimulator.push("github.com");
+        BrowserHistorySimulator.push("youtube.com");
+        BrowserHistorySimulator.push("facebook.com");
+        BrowserHistorySimulator.push("google.com");
+        BrowserHistorySimulator.push("Netflex.com");
+
+
+
+    }
+
+
+
+
+    }
--- src/EmployeeStackDemo.java
+++ src/EmployeeStackDemo.java
@@ -1,6 +1,6 @@
 import java.util.Stack;
 
-public class EmployeeStackDemo {
+public class BrowserHistorySimulator {
 
     public static void main(String[] args){
         Stack<String> EmployeeStackDemo = new Stack<>();
