494e8066d6b15dc3db16cc92257c0d0ab6300c55
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-23 11:09:08 +0400

    adding 6 employee records

--- src/EmployeeStackSearch.java
+++ src/EmployeeStackSearch.java
@@ -2,17 +2,18 @@ import java.util.Stack;
 
 public class EmployeeStackSearch {
     public static void main(String[] args){
-        Stack<String> EmployeeStackDemo = new Stack<>();
+
+        Stack<String> EmployeeStackSearch = new Stack<>();
 
         //Creating a stack to store employee actions
-        EmployeeStackDemo.push("Employee A submitted report");
-        EmployeeStackDemo.push("Employee B updating the data");
-        EmployeeStackDemo.push("Employee c adding the changes");
-        EmployeeStackDemo.push("Employee D completed the report");
-        EmployeeStackDemo.push("Employee E logging out");
-        EmployeeStackDemo.push("Employee F Approving the report");
+        EmployeeStackSearch.push("Employee A submitted report");
+        EmployeeStackSearch.push("Employee B updating the data");
+        EmployeeStackSearch.push("Employee c adding the changes");
+        EmployeeStackSearch.push("Employee D completed the report");
+        EmployeeStackSearch.push("Employee E logging out");
+        EmployeeStackSearch.push("Employee F Approving the report");
 
-
+        //Displaying the elements on stack
+        System.out.println("Stack after pushes: " + EmployeeStackSearch);
     }
-
-    }
+}
