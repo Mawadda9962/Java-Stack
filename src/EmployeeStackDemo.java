31d7917e126da805b9e248c164847593764c9ad8
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-23 10:57:24 +0400

    adding some comments

--- src/EmployeeStackDemo.java
+++ src/EmployeeStackDemo.java
@@ -5,6 +5,7 @@ public class EmployeeStackDemo {
     public static void main(String[] args){
         Stack<String> EmployeeStackDemo = new Stack<>();
 
+        //Creating a stack
         EmployeeStackDemo.push("Employee A submitted report");
         EmployeeStackDemo.push("Employee B updating the data");
         EmployeeStackDemo.push("Employee c adding the changes");
@@ -12,6 +13,7 @@ public class EmployeeStackDemo {
         EmployeeStackDemo.push("Employee E logging out");
 
         System.out.println("Stack after pushes: " + EmployeeStackDemo);
+
 
 
 
