9ed0e14516ae0497be0a895d80bd29daa8a95b38
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-26 11:13:32 +0400

    Devision process

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -5,10 +5,20 @@ public class DecimalToBinaryConverter {
 
         // Declaring a Stack variable with Integer type
         Stack<Integer> DecimalToBinaryConverter = new Stack<>();
-
-
         int num = 10;
         System.out.println(num);
+
+        int tem = num;
+
+        DecimalToBinaryConverter.push(tem % 2);
+        tem = tem / 2;
+
+        DecimalToBinaryConverter.push(tem % 2);
+        tem = tem / 2;
+
+
+
+
 
 
 
