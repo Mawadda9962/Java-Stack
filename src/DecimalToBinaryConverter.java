d900b3fc7f08a81c35e04fd0f86fef4c99501993
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-26 10:44:34 +0400

    Declaring a Stack variable with Integer type

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -3,18 +3,14 @@ import java.util.Stack;
 public class DecimalToBinaryConverter {
     public static void main(String[] args){
 
-        // Declaring a Stack variable with String type for employee actions
+        // Declaring a Stack variable with Integer type
         Stack<Integer> DecimalToBinaryConverter = new Stack<>();
 
 
+        int num = 10;
+        System.out.println(num);
 
 
-        //Adding 5 meaningful employee action records using push()
-        DecimalToBinaryConverter.push("Employee A logged into system");
-        DecimalToBinaryConverter.push("Employee B submitted report");
-        DecimalToBinaryConverter.push("Employee C updated database records");
-        DecimalToBinaryConverter.push("Employee D approved leave request");
-        DecimalToBinaryConverter.push("Employee E logged out of system");
 
 
 
