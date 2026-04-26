def29b97dc380f0a3758c9027114e5a69317e704
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-26 15:57:16 +0400

    Adding tow stacks

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -3,15 +3,52 @@ import java.util.Stack;
 public class TextEditorWithUndoRedo {
     public static void main(String[] args){
 
-        //Creating two stacks (undo and redo)
+        // Creating two stacks (undo and redo)
         Stack<String> undoStack = new Stack<>();
         Stack<String> redoStack = new Stack<>();
 
-
+        // Current text
         StringBuilder text = new StringBuilder();
 
+        // Type Hello
+        undoStack.push(text.toString());
+        text.append("Hello");
+        redoStack.clear();
+        System.out.println("Type: Hello");
+        display(text, undoStack, redoStack);
 
+        // Type World
+        undoStack.push(text.toString());
+        text.append(" World");
+        redoStack.clear();
+        System.out.println("Type: World");
+        display(text, undoStack, redoStack);
 
+        // Undo
+        if(!undoStack.isEmpty()){
+            redoStack.push(text.toString());
+            text = new StringBuilder(undoStack.pop());
+        } else {
+            System.out.println("Nothing to undo");
+        }
+        System.out.println("Undo");
+        display(text, undoStack, redoStack);
+
+        // Redo
+        if(!redoStack.isEmpty()){
+            undoStack.push(text.toString());
+            text = new StringBuilder(redoStack.pop());
+        } else {
+            System.out.println("Nothing to redo");
+        }
+        System.out.println("Redo");
+        display(text, undoStack, redoStack);
     }
 
+    // Display method
+    public static void display(StringBuilder text, Stack<String> undoStack, Stack<String> redoStack){
+        System.out.println(text);
+        System.out.println(undoStack);
+        System.out.println(redoStack);
+    }
 }
