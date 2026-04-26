b87aa791258b3637dccd8da2097d3ffecea613ab
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-26 16:12:56 +0400

    Closing tag

--- src/HtmlTagValidator.java
+++ src/HtmlTagValidator.java
@@ -13,7 +13,31 @@ public class HtmlTagValidator {
         Stack<String> stack = new Stack<>();
 
         for(int i = 0; i < html.length(); i++){
+            // check for opening <
             if(html.charAt(i) == '<'){
+                int end = html.indexOf('>', i);
+                String tag = html.substring(i + 1, end);
+
+                System.out.println("Tag: " + tag);
+
+                // closing tag
+                if(tag.startsWith("/")){
+
+                    String name = tag.substring(1);
+
+                    if(stack.isEmpty()){
+                        System.out.println("Error: no opening tag");
+                        return false;
+                    }
+
+                    String top = stack.pop();
+
+                    if(!top.equals(name)){
+                        System.out.println("Error: not matching");
+                        return false;
+                    }
+                }
+
 
             }
         }
