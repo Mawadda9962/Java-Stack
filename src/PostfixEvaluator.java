6fdf2661c9434b2bbdf8ba9e05f25ef3dc261587
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-26 14:02:25 +0400

    checking the digits on the string

--- src/PostfixEvaluator.java
+++ src/PostfixEvaluator.java
@@ -6,8 +6,39 @@ public class PostfixEvaluator {
         String num = " 3 5 +";
 
     }
+
     public static Integer evaluatePostfix(String expression){
-        Stack<Integer> PostfixEvaluator  = new Stack<>();
+
+        // Declaring a Stack variable with Integer type
+        Stack<Integer> stack = new Stack<>();
+
+        //Empty or null
+        if (expression == null || expression.isEmpty() ){
+            System.out.println("Empty expression");
+            return null;
+        }
+
+
+        for(int i = 0; i < expression.length(); i++){
+            char a = expression.charAt(i);
+
+            if(Character.isDigit(a)){
+                stack.push(Character.getNumericValue(a));
+            }else {
+
+                if(stack.size() < 2){
+                    System.out.println();
+                }
+
+
+
+            }
+
+
+        }
+
+
+
 
 
 
