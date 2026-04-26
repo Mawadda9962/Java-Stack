d2ea661fa99e3bcafbdf13244259f0729381f094
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-26 13:49:52 +0400

    adding integers

---
+++ src/PostfixEvaluator.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class PostfixEvaluator {
+    public static void main(String[] args){
+
+        String num = " 3 5 +";
+
+    }
+    public static Integer evaluatePostfix(String expression){
+        Stack<Integer> PostfixEvaluator  = new Stack<>();
+
+
+
+    }
+}
