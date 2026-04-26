44b4f4d1465c594888949adeb71464c8dfcbe7fb
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-26 10:42:15 +0400

    Creating a stack

---
+++ src/DecimalToBinaryConverter.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class DecimalToBinaryConverter {
+    public static void main(String[] args){
+
+        // Declaring a Stack variable with String type for employee actions
+        Stack<Integer> DecimalToBinaryConverter = new Stack<>();
+
+
+
+
+        //Adding 5 meaningful employee action records using push()
+        DecimalToBinaryConverter.push("Employee A logged into system");
+        DecimalToBinaryConverter.push("Employee B submitted report");
+        DecimalToBinaryConverter.push("Employee C updated database records");
+        DecimalToBinaryConverter.push("Employee D approved leave request");
+        DecimalToBinaryConverter.push("Employee E logged out of system");
+
+
+
+
+    }
+}
