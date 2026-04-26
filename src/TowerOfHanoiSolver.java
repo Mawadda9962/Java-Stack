e5a1ee8fc6f303cbaea611df7f73023c34b7702d
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-26 16:44:54 +0400

    declaring the stack name

--- src/TowerOfHanoiSolver.java
+++ src/TowerOfHanoiSolver.java
@@ -1,12 +1,14 @@
 import java.util.Stack;
 
 public class TowerOfHanoiSolver {
-    public static void main(String[] args){
 
         static Stack<Integer> A = new Stack<>();
         static Stack<Integer> B = new Stack<>();
         static Stack<Integer> C = new Stack<>();
         static int count = 0;
+
+
+        public static void main(String[] args){
 
 
     }
