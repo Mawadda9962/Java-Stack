6da9edb62a0a7ee1c1341957f607697344ce3faa
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-26 15:53:37 +0400

    Adding tow stacks

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -3,9 +3,13 @@ import java.util.Stack;
 public class TextEditorWithUndoRedo {
     public static void main(String[] args){
 
-        //
+        //Creating two stacks (undo and redo)
         Stack<String> undoStack = new Stack<>();
         Stack<String> redoStack = new Stack<>();
+
+
+        StringBuilder text = new StringBuilder();
+
 
 
     }
