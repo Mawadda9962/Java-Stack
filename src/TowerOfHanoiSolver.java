a87f4a0cbcdbaa5228fae79a3cbe3efa81111efb
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-26 16:43:26 +0400

    declaring the stack

--- src/TowerOfHanoiSolver.java
+++ src/TowerOfHanoiSolver.java
@@ -1,5 +1,13 @@
+import java.util.Stack;
+
 public class TowerOfHanoiSolver {
     public static void main(String[] args){
+
+        static Stack<Integer> A = new Stack<>();
+        static Stack<Integer> B = new Stack<>();
+        static Stack<Integer> C = new Stack<>();
+        static int count = 0;
+
 
     }
 }
