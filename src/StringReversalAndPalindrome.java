afa3879c3cd239373234327bcdcea10d507bd67a
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-26 09:22:51 +0400

    adding the first String

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -1,5 +1,15 @@
+import java.util.Stack;
+
 public class StringReversalAndPalindrome {
     public static void main(String[] args){
+
+        Stack<String> EmployeeStackSearch = new Stack<>();
+
+        //First string
+        String title1 = "madam";
+
+
+
 
 
 
