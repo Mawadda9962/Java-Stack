62632ea17ef82748b5ab07036ce5e94103d4965b
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-28 14:34:52 +0400

    Adding Two stacks: one for incoming data, one for outgoing data

---
+++ src/QueueUsingTwoStacks.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+
+public class QueueUsingTwoStacks {
+    public static void main(String[] args){
+
+        private Stack<T> input = new Stack<>();
+        private Stack<T> output = new Stack<>();
+
+    }
+
+}
