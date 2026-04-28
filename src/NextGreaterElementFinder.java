812c10a5655a71b8f4ec77109cb56563a4e29e1e
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-28 13:21:34 +0400

    check if current element is greater

--- src/NextGreaterElementFinder.java
+++ src/NextGreaterElementFinder.java
@@ -1,43 +1,39 @@
 import java.util.Stack;
 
 public class NextGreaterElementFinder {
-    public static void main (String[] args){
 
+    public static void main(String[] args) {
 
         int[] arr = {13, 7, 6, 12};
-        int[] result = nextGrater(arr);
+        int[] result = nextGreater(arr);
 
-        for(int i = 0; i < arr.length; i++){
-            System.out.println(arr[i] + " " + result[i]);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " → " + result[i]);
         }
-
     }
 
-    //Simple Next Grater function
-    public static int[] nextGrater(int[] arr){
+    // Simple Next Greater function
+    public static int[] nextGreater(int[] arr) {
 
         int[] result = new int[arr.length];
-        Stack<String> stack = new Stack<>();
 
-        //initialize all as -1
-        for(int i = 0; i < arr.length; i++){
+        Stack<Integer> stack = new Stack<>();
+
+        // initialize all as -1
+        for (int i = 0; i < arr.length; i++) {
             result[i] = -1;
-
         }
 
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
 
-            //check if current element is greater
+            // check if current element is greater
             while (!stack.isEmpty() && arr[i] > arr[stack.peek()]) {
-
-
+                result[stack.pop()] = arr[i];
             }
 
-
+            stack.push(i);
         }
 
-
+        return result;
     }
-
-
 }
