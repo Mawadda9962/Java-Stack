862f45732e8862edbe0dc149d118b526b40c3c01
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-28 15:28:08 +0400

    // Create an instance of our queue for Integers

--- src/QueueUsingTwoStacks.java
+++ src/QueueUsingTwoStacks.java
@@ -1,10 +1,14 @@
 import java.util.Stack;
 
-public class QueueUsingTwoStacks {
-    public static void main(String[] args){
+public class QueueUsingTwoStacks<T> {
 
+        //Adding Two stacks one for incoming data, one for outgoing data
         private Stack<T> input = new Stack<>();
         private Stack<T> output = new Stack<>();
+
+    public static void main(String[] args){
+        // Create an instance of our queue for Integers
+        QueueUsingTwoStacks<Integer> queue = new QueueUsingTwoStacks
 
     }
 
