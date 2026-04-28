e85dec7c1ca9ffea1d6f7e3f10f55bfa58f94dc0
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-28 12:36:38 +0400

    clear stacks before each evaluation

---
+++ src/InfixToPostfixConverter.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class InfixToPostfixConverter {
+
+    static Stack<Double> numbers = new Stack<>();
+    static Stack<Character> operators = new Stack<>();
+
+
+    public static void main(String[] args){
+
+
+
+    }
+
+    public static String evaluate (String expression){
+        numbers.clear();
+        operators.clear();
+
+
+    }
+
+
+}
