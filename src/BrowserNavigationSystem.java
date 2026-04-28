4a6c889e33d2f78b8b6d12cd1db5b687a4216a9a
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-28 09:28:15 +0400

    Adding stack for back and forword navigation

---
+++ src/BrowserNavigationSystem.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class BrowserNavigationSystem {
+
+    static Stack<String> backStack = new Stack<>();
+    static Stack<String> forwardStack = new Stack<>();
+
+
+    public static void main(String[] args){
+
+    }
+}
