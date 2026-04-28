2056b539116ed43dbfdb226aec759d6bf4051bf1
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-28 09:30:43 +0400

    Storing current page

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -2,8 +2,13 @@ import java.util.Stack;
 
 public class BrowserNavigationSystem {
 
+    //Adding Stacks for back and forward navigation
     static Stack<String> backStack = new Stack<>();
     static Stack<String> forwardStack = new Stack<>();
+
+    // Store current page
+    static String currentPage = "Home";
+
 
 
     public static void main(String[] args){
