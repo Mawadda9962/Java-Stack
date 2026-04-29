ad493eea71b3105072e9310beace55a421a3e7c2
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-29 09:18:20 +0400

    // Stack to track minimum values

--- src/MinStack.java
+++ src/MinStack.java
@@ -2,7 +2,12 @@ import java.util.Stack;
 
 public class MinStack {
 
+    // Add Stack to store all elements
     static Stack<Integer> elementsStack = new Stack<>();
+
+    // Stack to track minimum values
+    static Stack<Integer> minimumStack = new Stack<>();
+
 
 
 }
