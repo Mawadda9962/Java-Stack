9c0c47b156d6b5a42fe11f69b7383e4dc91b12dc
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-29 10:01:51 +0400

    Adding a new stack

--- src/StackSorter.java
+++ src/StackSorter.java
@@ -1,5 +1,15 @@
+import java.util.Stack;
 public class StackSorter  {
     public static void main(String[] args){
+
+        Stack<Integer> originalStack = new Stack<>();
+
+        originalStack.push(3);
+        originalStack.push(2);
+        originalStack.push(4);
+        originalStack.push(1);
+        originalStack.push(5);
+
 
     }
 
