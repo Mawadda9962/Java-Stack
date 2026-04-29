ee53e1b5315a11c1496372059df96bb0fc741bf7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-29 09:31:24 +0400

    Outputting results directly

--- src/MinStack.java
+++ src/MinStack.java
@@ -1,13 +1,19 @@
 import java.util.Stack;
 
 public class MinStack {
+    // Primary storage and the tracking stack for minimum values
+    static Stack<Integer> primaryStack = new Stack<>();
+    static Stack<Integer> trackMinStack = new Stack<>();
 
-    // Add Stack to store all elements
-    static Stack<Integer> elementsStack = new Stack<>();
+    public static void main(String[] args) {
+        push(5);
+        push(3);
+        push(10);
+        push(30);
+        push(2);
 
-    // Stack to track minimum values
-    static Stack<Integer> minimumStack = new Stack<>();
-
-
-
-}
+        // Outputting results directly
+        System.out.println("Getmin : " + GetMin());
+        System.out.println("Popped : " + pop());
+        System.out.println("New Getmin : " + GetMin());
+    }
